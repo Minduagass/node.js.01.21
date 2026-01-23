4eeea1d4af9fee320f23665c2bb5714b5add0fea
Mindaugas <m.gudziunas24@gmail.com>
2026-01-23 13:27:41 +0200

    added auth

--- src/router/recipe.js
+++ src/router/recipe.js
@@ -7,19 +7,20 @@ import {
   deleteRecipeById,
   getRandomRecipe,
 } from "../controller/recipe.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/recipes", getAllRecipes);
+router.get("/recipes", auth, getAllRecipes);
 
-router.get("/recipes/random", getRandomRecipe);
+router.get("/recipes/random", auth, getRandomRecipe);
 
-router.get("/recipes/:id", getRecipeById);
+router.get("/recipes/:id", auth, getRecipeById);
 
-router.post("/recipes", insertRecipe);
+router.post("/recipes", auth, insertRecipe);
 
-router.put("/recipes/:id", updateRecipeById);
+router.put("/recipes/:id", auth, updateRecipeById);
 
-router.delete("/recipes/:id", deleteRecipeById);
+router.delete("/recipes/:id", auth, deleteRecipeById);
 
 export default router;
